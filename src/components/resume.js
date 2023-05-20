397c414827a13101fbff63400b46f166d0feae03
Bronson Wardle <bronson.wardle@gmail.com>
2023-05-20 12:07:44 -0400

    updated my resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,16 +1,16 @@
 import React from "react";
-// import resume from "../imgs/BronsonWresume.pdf";
+import resume from "../imgs/resume.pdf";
 
 function Resume(){
   return(
     <div className='columns'>
       <div className='column'>
         <a className="button is-primary"
-        href={"no file here"} download>
+        href={resume} download>
           <span className="icon">
             <i className="fas fa-download"></i>
           </span>
-          <span>View My outdated Resume</span>
+          <span>Download my resume</span>
         </a>
       </div>
       <div className='column'>
